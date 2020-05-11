d1e7ed8b1a8858054b7c1e0cd4bb3a531c6ff85f
Meg Boehlert <boehlertm@gmail.com>
2020-05-10 22:53:35 -0400

    feat: dashboard or home page for loggedInUser
    * Shows unanswered polls and answered polls sorted by time most recent
    * WIP: allow user to toggle answered/unanswered

---
+++ src/components/Dashboard.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import Question from './Question'
+
+class Dashboard extends Component {
+  render() {
+    const { unansweredIds, answeredIds } = this.props
+    return (
+      <div className='questions'>
+        <ul className='unanswered'>
+          <h2>Unanswered Questions</h2>
+          {unansweredIds && unansweredIds.map((qid) => (
+            <li key={qid}>
+              <Question qid={qid} answered={false}/>
+            </li>
+          ))}
+        </ul>
+        <ul className='answered'>
+          <h2>Answered Questions</h2>
+          {answeredIds && answeredIds.map((qid) => (
+            <li key={qid}>
+              <Question qid={qid} answered={true}/>
+            </li>
+          ))}
+        </ul>
+      </div>
+    )
+  }
+}
+
+function mapStateToProps({ loggedInUser, questions }) {
+  let unanswered = [];
+  let answered = [];
+  console.log(questions)
+
+  Object.values(questions).forEach((question) => {
+    if (question.optionOne.votes.find(vote => vote === loggedInUser)
+      || question.optionTwo.votes.find(vote => vote === loggedInUser)) {
+      return answered.push(question)
+    } else {
+      unanswered.push(question)
+    }
+  })
+
+  return {
+    unansweredIds: unanswered
+                    .sort((a,b) => b.timestamp - a.timestamp)
+                    .map(question => question.id),
+    answeredIds: answered
+                  .sort((a,b) => b.timestamp - a.timestamp)
+                  .map(question => question.id),
+  }
+}
+
+export default connect(mapStateToProps)(Dashboard)

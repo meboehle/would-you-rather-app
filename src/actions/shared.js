d910037ea08c32156a1113e6dac85684330a3cc5
Meg Boehlert <boehlertm@gmail.com>
2020-05-10 22:46:24 -0400

    feat: shared action to receive initial data (users and questions)

---
+++ src/actions/shared.js
@@ -0,0 +1,16 @@
+import { getInitData } from '../utils/api'
+import { receiveUsers } from '../actions/users'
+import { receiveQuestions } from '../actions/questions'
+import { showLoading, hideLoading } from 'react-redux-loading'
+
+export function handleInitData () {
+  return (dispatch) => {
+    dispatch(showLoading())
+    return getInitData()
+      .then(({ users, questions }) => {
+        dispatch(receiveUsers(users))
+        dispatch(receiveQuestions(questions))
+        dispatch(hideLoading())
+      })
+  }
+}

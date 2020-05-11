a6970f98a327f59787197827265db20dbddf8911
Meg Boehlert <boehlertm@gmail.com>
2020-05-11 19:12:54 -0400

    devtools: allow state to show in redux devtools

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
-import { createStore } from 'redux'
+import { createStore, compose } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
 import middleware from './middleware'
 
-const store = createStore(reducer, middleware)
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = createStore(reducer, composeEnhancer(middleware))
 
 ReactDOM.render(
   <Provider store={store}>

dc3df09d85b9a93f4927e13925af69656028fff7
Meg Boehlert <boehlertm@gmail.com>
2020-05-12 11:49:57 -0400

    refactor: moved loadingbar to top and put welcome component inside navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
+import { Route, withRouter , Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { handleInitData } from '../actions/shared'
 import Login from './Login'
@@ -7,40 +7,35 @@ import Dashboard from './Dashboard'
 import NavBar from './NavBar'
 import QuestionDetails from './QuestionDetails'
 import NewQuestion from './NewQuestion'
-import Welcome from './Welcome'
 // import NotFound from './404'
 import LoadingBar from 'react-redux-loading'
 
 class App extends Component {
   componentDidMount() {
     this.props.dispatch(handleInitData())
+    // if (!this.props.authedUser) {
+    //   this.props.history.push('/login')
+    // }
   }
 
   render() {
-    const { authedUser } = this.props
     return (
-      <Router>
         <div>
+          <LoadingBar/>
           <h1 className='game title'>Would You Rather?</h1>
           <div>
-            {!authedUser &&
-              <h3 className='login title'>
-                Pick a User to Begin Playing
-              </h3>}
-            {authedUser && <NavBar/>}
-            <LoadingBar/>
-            {authedUser && <Welcome/>}
-            {!authedUser ? <Redirect to='/login' /> : <Redirect to='/' />}
-            <Route path='/login' component={Login} />
+            
+          {!this.props.authedUser ? <Redirect to='/login' /> : <Redirect to='/' />}
+          <Route path='/login' component={Login} />
             {this.props.loading === true ? null :
               <div>
+                <NavBar/>
                 <Route path='/' exact component={Dashboard} />
                 <Route path='/question/:id' component={QuestionDetails} />
                 <Route path='/addQuestion' component={NewQuestion} />
               </div>}
           </div>
         </div>
-      </Router>
     )
   }
 }
@@ -52,4 +47,4 @@ function mapStateToProps ({authedUser}) {
   }
 }
 
-export default connect(mapStateToProps)(App)
+export default withRouter(connect(mapStateToProps)(App))

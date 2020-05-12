4c76b1849abd4cfe258a8d49ebc58df2fceeaf79
Meg Boehlert <boehlertm@gmail.com>
2020-05-12 11:48:51 -0400

    chore: moved browserrouter from app to index

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom'
 import './index.css';
 import App from './components/App';
 import { createStore, compose } from 'redux'
@@ -12,7 +13,7 @@ const store = createStore(reducer, composeEnhancer(middleware))
 
 ReactDOM.render(
   <Provider store={store}>
-    <App/>
+    <BrowserRouter><App/></BrowserRouter>
   </Provider>,
   document.getElementById('root')
 );

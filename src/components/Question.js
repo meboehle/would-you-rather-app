b51fab52602fb4a831de368a8f74d6d969eef3fe
Meg Boehlert <boehlertm@gmail.com>
2020-05-13 19:38:17 -0400

    refactor: swapped class for functional component, removed unused lifecycle hooks/code

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,54 +1,41 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { connect } from 'react-redux'
 import { formatDate } from '../utils/helpers'
-import { Link, withRouter } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
-class Question extends Component {
-  // componentDidMount () {
-  //   this.props.history.push(`/question/${this.props.question.id}`)
-  // }
+const Question = ({users, question}) => {
+  const {
+    id, author, optionOne, optionTwo, timestamp
+  } = question
 
-  render() {
-    const { users, question } = this.props
+  const { avatarURL, name } = Object.values(users).find((user) => user.id === author)
+  const date = formatDate(timestamp)
 
-    if (question === null) {
-      return <p>This question doesn't exist</p>
-    }
-
-    const {
-      id, author, optionOne, optionTwo, timestamp
-    } = question
-
-    const { avatarURL, name } = Object.values(users).find((user) => user.id === author)
-    const date = formatDate(timestamp)
-
-    return (
-      <Link to={`/question/${id}`} className='question'>
-        <div className='question-author'>
-            <img
-              src={avatarURL}
-              alt={`Avatar of ${name}`}
-              className='avatar-question'/>{name}
-        </div>
-        <div className='date'>{date}</div>
-        <div className='question-container'>
-          <div className='wyr-or'>Would you rather...</div>
-          <span>{optionOne.text}</span>
-          <div className='wyr-or'> or </div>
-          <span>{optionTwo.text}</span>
-        </div>
-      </Link>
-    )
-  }
+  return (
+    <Link to={`/question/${id}`} className='question'>
+      <div className='question-author'>
+          <img
+            src={avatarURL}
+            alt={`Avatar of ${name}`}
+            className='avatar-question'/>{name}
+      </div>
+      <div className='date'>{date}</div>
+      <div className='question-container'>
+        <div className='wyr-or'>Would you rather...</div>
+        <span>{optionOne.text}</span>
+        <div className='wyr-or'> or </div>
+        <span>{optionTwo.text}</span>
+      </div>
+    </Link>
+  )
 }
 
-function mapStateToProps({ authedUser, questions, users }, { qid }) {
+function mapStateToProps({ questions, users }, { qid }) {
   const question = questions[qid]
   return {
-    authedUser,
     question,
     users,
   }
 }
 
-export default withRouter(connect(mapStateToProps)(Question))
+export default connect(mapStateToProps)(Question)

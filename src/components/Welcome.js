ffd0342af773072f387c11175359ca53b2687efc
Meg Boehlert <boehlertm@gmail.com>
2020-05-13 19:38:51 -0400

    refactor: swapped class for functional component

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,28 +1,22 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { connect } from 'react-redux'
 
-class Welcome extends Component {
-
-  render() {
-    const { authedUser, users } = this.props
-    const user = Object.values(users).find(user => user.id === authedUser)
-
-    return (
-      <div className='welcome'>
-        <img
-          src={user.avatarURL}
-          alt={`Avatar of ${user.name}`}
-          className='avatar-welcome'/>
-        Welcome, {user.name}!
-      </div>
-    )
-  }
+const Welcome = ({user}) => {
+  return (
+    <div className='welcome'>
+      <img
+        src={user.avatarURL}
+        alt={`Avatar of ${user.name}`}
+        className='avatar-welcome'/>
+      Welcome, {user.name}!
+    </div>
+  )
 }
 
 function mapStateToProps ({authedUser, users}) {
+  const user = Object.values(users).find(user => user.id === authedUser)
   return {
-    authedUser,
-    users
+    user
   }
 }
 

cef7d3c2ecbed5d05fe4276ae1be3670c56bcfb0
Meg Boehlert <boehlertm@gmail.com>
2020-10-07 08:58:19 -0400

    Updates

---
+++ precache-manifest.f61d4fdb4d6d6852ffe048e65c0f83f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6da0cf9d86eb08403e996e18b302893",
+    "url": "/would-you-rather/index.html"
+  },
+  {
+    "revision": "3b6146eea76f0a554948",
+    "url": "/would-you-rather/static/css/main.d48a3b0c.chunk.css"
+  },
+  {
+    "revision": "6f36ede59127f4702431",
+    "url": "/would-you-rather/static/js/2.c407f34a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/would-you-rather/static/js/2.c407f34a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b6146eea76f0a554948",
+    "url": "/would-you-rather/static/js/main.769b9a0a.chunk.js"
+  },
+  {
+    "revision": "490c56574d35ec3e0b56",
+    "url": "/would-you-rather/static/js/runtime-main.03b33da1.js"
+  }
+]);
